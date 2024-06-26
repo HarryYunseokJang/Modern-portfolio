66796ad9599cc32e99ce5f2903ee5af35e024fa5
HYJ <remnantseok@naver.com>
2024-06-26 14:57:34 +0000

    Set up the project

---
+++ portfolio/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black">
+      <div>
+        <h1 className="text-white">Hello, Portfolio!</h1>
+      </div>
+    </main>
+  );
+}
+ 
